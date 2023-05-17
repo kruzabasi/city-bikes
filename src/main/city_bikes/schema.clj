9cedf8b8b8d6b7f2370e621afee77fb34b331498
Kruzabasi <kruzabasi@gmail.com>
2023-05-17 13:09:06 +0100

    define trips schema

--- src/main/city_bikes/schema.clj
+++ src/main/city_bikes/schema.clj
@@ -10,3 +10,34 @@
     :db/valueType   :db.type/string
     :db/cardinality :db.cardinality/one
     :db/doc         "Station Name"}])
+
+(def trip-schema
+  [{:db/ident       :trip/departure-time
+    :db/valueType   :db.type/instant
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Trip departure time"}
+   
+   {:db/ident       :trip/return-time
+    :db/valueType   :db.type/instant
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Trip return time"}
+   
+   {:db/ident       :trip/departure-station
+    :db/valueType   :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Trip departure Station"}
+   
+   {:db/ident       :trip/return-station
+    :db/valueType   :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Trip return Station"}
+   
+   {:db/ident       :trip/distance
+    :db/valueType   :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Distance of Trip in meters"}
+   
+   {:db/ident       :trip/duration
+    :db/valueType   :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Duration of Trip in seconds"}])
