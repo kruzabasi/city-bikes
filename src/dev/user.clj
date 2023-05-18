bcf4ceaac402c388bcda0338dd3e87bbb3808bdf
Kruzabasi <kruzabasi@gmail.com>
2023-05-18 14:38:37 +0100

    read csv files

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,6 +1,8 @@
 (ns user
   (:require [integrant.repl :as ig-repl]
             [city-bikes.server :as server]
+            [clojure.java.io :as io]
+            [clojure.data.csv :as csv]
             [datomic.api :as d]))
 
 (ig-repl/set-prep!
@@ -15,16 +17,22 @@
   []
   (ig-repl/halt))
 
-(def db-uri "datomic:dev://localhost:4334/city-bike")
+(defn read-csv-data
+  "Reads a csv file and returns the data without headers"
+  [file]
+  (let [data (-> file io/resource slurp csv/read-csv rest)]
+    data))
 
-(comment 
- (defn create-client
-  []
-  (d/client {:server-type :dev-local
-             :system "dev"})))
+(comment
+  (def db-uri "datomic:dev://localhost:4334/city-bike")
+
 
 (defn create-db
   []
   (d/create-database db-uri))
 
-(def conn (d/connect db-uri))
+  (def conn (d/connect db-uri))
+
+
+  (defn initiate-schema [schema-data]
+    (d/transact conn schema-data)))
