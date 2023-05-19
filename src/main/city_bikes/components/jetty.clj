9fc746ae0aa83419fc4eac796930cfaa7bf4765d
Kruzabasi <kruzabasi@gmail.com>
2023-05-19 20:26:13 +0100

    refactor jetty server

---
+++ src/main/city_bikes/components/jetty.clj
@@ -0,0 +1,16 @@
+(ns city-bikes.components.jetty
+  (:require [integrant.core :as ig]
+            [ring.adapter.jetty :as jetty]))
+
+(defmethod ig/init-key ::server
+  [_ {:keys [handler port]}]
+  (println (str "\nStarting jetty server..."))
+  
+  (println (str "\nServer running on port " port))
+  
+  (jetty/run-jetty handler {:port port
+                            :join? false}))
+
+(defmethod ig/halt-key! ::server
+  [_ jetty]
+  (.stop jetty))
--- src/main/city_bikes/server.clj
+++ src/main/city_bikes/server.clj
@@ -1,23 +1,7 @@
 (ns city-bikes.server
-  (:require [integrant.core :as ig]
-            [ring.adapter.jetty :as jetty]
-            [ring.util.response :as rr])
-  (:import  [org.eclipse.jetty.server Server]))
+  (:require [integrant.core :as ig]))
 
-(def config 
-  {:adapter/jetty {:port 8080 :handler (ig/ref :handler/greet)}
-   :handler/greet {:name "City Bike"}})
-
-(defmethod ig/init-key :handler/greet
- [_ opts]
- (fn [_]
-   (rr/response "Hello City-Bike")))
-
-(defmethod ig/init-key :adapter/jetty
-  [_ {:keys [handler port]}]
-  (jetty/run-jetty handler {:port  port
-                            :join? false}))
-
-(defmethod ig/halt-key! :adapter/jetty 
-  [_ ^Server server]
-  (.stop server))
+(defmethod ig/init-key ::app
+  [_ config]
+  (println "\nStarting app..")
+  (comment "Handle Routes Here!"))
