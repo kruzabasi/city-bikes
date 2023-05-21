1880522b721caf439bf9edb1359caab322c01163
Kruzabasi <kruzabasi@gmail.com>
2023-05-21 21:27:51 +0100

    enable app config

--- src/main/city_bikes/server.clj
+++ src/main/city_bikes/server.clj
@@ -1,7 +1,12 @@
 (ns city-bikes.server
-  (:require [integrant.core :as ig]))
+  (:require [integrant.core :as ig]
+            [city-bikes.router :as router]))
+
+(defn app
+  [sys]
+  (router/routes sys))
 
 (defmethod ig/init-key ::app
   [_ config]
   (println "\nStarting app..")
-  (comment "Handle Routes Here!"))
+  (app config))
