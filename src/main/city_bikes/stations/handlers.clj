e7066f811f182c3a64e5ffe672012c4960b021a7
Kruzabasi <kruzabasi@gmail.com>
2023-06-01 16:33:12 +0100

    get a single stations data

--- src/main/city_bikes/stations/handlers.clj
+++ src/main/city_bikes/stations/handlers.clj
@@ -11,3 +11,14 @@
                                     :station/name])
                        :where [?e :station/id]]
                      (d/db conn))}))
+
+(defn get-single-station
+  [{:keys [parameters sys]}]
+  (let [conn       (-> sys :datomic :conn)
+        station-id (-> parameters :path :station-id Long/parseLong)]
+    (rr/response {:data
+                  (d/q '[:find (pull ?e [*])
+                         :in $ ?station-id
+                         :where [?e :station/id ?station-id]]
+                       (d/db conn)
+                       station-id)})))
--- src/main/city_bikes/stations/routes.clj
+++ src/main/city_bikes/stations/routes.clj
@@ -1,10 +1,16 @@
 (ns city-bikes.stations.routes
-  (:require [city-bikes.stations.handlers :refer [get-all-stations]]))
+  (:require [city-bikes.stations.handlers :refer [get-all-stations get-single-station]]))
 
 (def routes
   ["/station"
    [""
-    {:get {:handler     get-all-stations
+    {:get {:handler    get-all-stations
            :responses  {201 {:body nil?}}
            :parameters {}
-           :summary    "Fetch list of Stations"}}]])
+           :summary    "Fetch list of Stations"}}]
+   ["/:station-id"
+    [""
+     {:get {:handler    get-single-station
+            :responses  {201 {:body nil?}}
+            :parameters {:path {:station-id string?}}
+            :summary    "Fetch a Single Stations Data"}}]]])
